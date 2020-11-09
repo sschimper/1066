d4a43995832e97b7db6437be2d622d673ba5b8b5
sebastian <sebastianschimper@gmail.com>
2020-11-09 20:37:22 +0100

    Button Behaviour added: weird bug when changing text colour

--- Assets/Scripts/UI/ButtonBehaviour.cs
+++ Assets/Scripts/UI/ButtonBehaviour.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ButtonBehaviour : MonoBehaviour
+[RequireComponent(typeof(Button))]
+public class ButtonBehaviour : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private Button button;
+    private TextMeshProUGUI buttonText;
+    private Color defaultColor;
+    public Color hoveredColor;
+
+    private AudioSource buttonHoverSource;
+    private AudioSource buttonClickSource;
+
+    private void Start()
     {
-        
+        button = GetComponent<Button>();
+        buttonText = button.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        defaultColor = buttonText.color;
+        hoveredColor = new Color(218, 165, 32); // golden tone
+        // hoveredColor = new Color(225, 0, 0);
+
+        buttonHoverSource = GameObject.Find("Button Hover Sound").GetComponent<AudioSource>();
+        buttonClickSource = GameObject.Find("Button Click Sound").GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        if(buttonText.color != hoveredColor) buttonText.color = hoveredColor;
+        buttonHoverSource.PlayOneShot(buttonHoverSource.clip); 
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (buttonText.color != defaultColor) buttonText.color = defaultColor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonClickSource.PlayOneShot(buttonClickSource.clip);
     }
 }
