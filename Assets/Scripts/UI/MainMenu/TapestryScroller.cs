7fa3d72f4d43bff80cb869e93a28ce36e218c3ae
sebastian <sebastianschimper@gmail.com>
2020-11-09 19:19:57 +0100

    Main Menu: Added Tapestry Scroller

---
+++ Assets/Scripts/UI/MainMenu/TapestryScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapestryScroller : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public float thresholdPosX = -5876.0f;
+    private RectTransform rectTransform;
+
+    void Update()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        if (rectTransform.localPosition.x < thresholdPosX)
+        {
+            Vector3 startPos = new Vector3(thresholdPosX * (-1), 0);
+            rectTransform.localPosition = startPos;
+        }
+        else
+        {
+            Vector3 offset = new Vector3((-1) * speed, 0);
+            rectTransform.localPosition += offset;
+        }     
+    }
+}
